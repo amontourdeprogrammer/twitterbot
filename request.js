9a37f11da6c67b1fb0676cf2121a6c97bbf69813
meo84 <oopsiday@gmail.com>
2017-03-10 12:25:57 +0100

    select one exhibition to tweet with basic info

--- request.js
+++ request.js
@@ -4,17 +4,34 @@ request(
     { url: 'https://api.paris.fr/api/data/2.2/QueFaire/get_events/?token=4f3ff60a873f39938f7f00c43dad49e9e94a83a044f539de962299bbc09630d2&categories=exposition&tags=&start=&end=0&offset=&limit=' },
     function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        var exhibitions = JSON.parse(body).data;
-        console.log('last event date format:', typeof exhibitions[0].evenements.realDateEnd);
-        console.log('test_deadline_in_21_days', deadline_by(21));
-        console.log('test_first_exhibition_closing', close_before(exhibitions[0], deadline_by(150)));
-        console.log('test_exhibitions_closing_in_21_days', exhibitions_ending_soon(exhibitions, deadline_by(21)));
+        select_exhibition_to_tweet(body);
       } else {
         console.log('error:', error);
         console.log('statusCode:', response && response.statusCode);
       }
     }
-    )
+    );
+
+function select_exhibition_to_tweet(body) {
+  var exhibitions = JSON.parse(body).data;
+  var exhibitions_ending_soon_ = exhibitions_ending_soon(exhibitions, deadline_by(21));
+  var exhibition_to_tweet = exhibitions_ending_soon_[0];
+  //tests
+  //console.log('last event date format:', typeof exhibitions[0].evenements.realDateEnd);
+  //console.log('test_deadline_in_21_days', deadline_by(21));
+  //console.log('test_first_exhibition_closing', close_before(exhibitions[0], deadline_by(150)));
+  //console.log('test_exhibitions_closing_in_21_days', exhibitions_ending_soon(exhibitions, deadline_by(21)));
+  //console.log('exhibition to tweet', exhibition_to_tweet);
+  console.log('chosen exhibition tweet content', exhibition_summary(exhibition_to_tweet));
+}
+
+function exhibition_summary(exhibition) {
+  return {
+    "title": exhibition.title,
+    "url": exhibition.shortenedUrl,
+    "desc": exhibition.leadText
+  };
+}
 
 function deadline_by(nb_days) {
   var today = new Date ();
@@ -22,6 +39,7 @@ function deadline_by(nb_days) {
   deadline.setDate(today.getDate() + nb_days);
   return deadline;
 }
+
 function exhibitions_ending_soon(all_exhibitions, deadline) {
   var exhibitions_ending_soon = [];
   all_exhibitions.forEach(function(exhibition) {
@@ -37,4 +55,3 @@ function close_before(exhibition, deadline) {
   exhibition_closing_date = new Date (exhibition.evenements.realDateEnd);
   return exhibition_closing_date <= deadline;
 }
-
